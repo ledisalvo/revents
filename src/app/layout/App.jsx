18be636af373d5114072afeeace8fa8e43c6c9cc
Leonardo Adrian Di Salvo <ledisalvo@gmail.com>
2023-01-11 22:12:37 -0300

    Create EventListAttendee, EventListItem and EventList components

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import EventDashboard from '../../features/events/eventDashboard/eventDashboard';
+import EventDashboard from '../../features/events/eventDashboard/EventDashboard';
 import NavBar from '../../features/nav/NavBar';
 import { Container } from 'semantic-ui-react';
 
---
+++ src/features/events/eventDashboard/EventList.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import EventListItem from './EventListItem';
+
+const EventList = () => {
+  return (
+    <>
+      <EventListItem />
+      <EventListItem />
+      <EventListItem />
+      <EventListItem />
+      <EventListItem />
+      <EventListItem />
+    </>
+  );
+};
+
+export default EventList;
