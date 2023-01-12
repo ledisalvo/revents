42d9cc31d54faaf884cab2b123a6fca974a5e26e
Leonardo Adrian Di Salvo <ledisalvo@gmail.com>
2023-01-11 21:51:38 -0300

    Create NavBar component

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import EventDashboard from '../../features/events/eventDashboard/eventDashboard';
+import NavBar from '../../features/nav/NavBar';
+import { Container } from 'semantic-ui-react';
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Container className='main'>
+        <EventDashboard />
+      </Container>
+    </div>
+  );
+}
+
+export default App;
