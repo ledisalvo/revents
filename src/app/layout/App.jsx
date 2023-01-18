fd93e660d1dea1b68ad4b288fe6f570d2818820c
Leonardo Adrian Di Salvo <ledisalvo@gmail.com>
2023-01-18 11:14:49 -0300

    End of section 5

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,36 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
 import NavBar from '../../features/nav/NavBar';
 import { Container } from 'semantic-ui-react';
-import { Route, Routes, useNavigate } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import EventDashboard from '../../features/events/eventDashboard/EventDashboard';
 import EventDetailedPage from '../../features/events/eventDetailed/EventDetailedPage';
 import EventForm from '../../features/events/eventForm/EventForm';
 
 const App = () => {
-  const [formOpen, setFormOpen] = useState(false);
-  const [selectedEvent, setSelectedEvent] = useState(null);
-
-  function handleSelectedEvent(event) {
-    setSelectedEvent(event);
-    setFormOpen(true);
-  }
-
-  function handleCreateFormOpen() {
-    setSelectedEvent(null);
-    setFormOpen(true);
-  }
-
-  const navigate = useNavigate();
-  console.log('navigate: ' + navigate('/'));
-
   return (
     <>
-      <NavBar setFormOpen={handleCreateFormOpen} />
+      <NavBar />
       <Container className='main'>
         <Routes>
           <Route path='/events' element={<EventDashboard />} />
           <Route path='/events/:id' element={<EventDetailedPage />} />
-          <Route path='/createEvent' element={<EventForm />} />
+          <Route
+            // path={['/createEvent', '/manage/:id']}
+            path='/createEvent'
+            element={<EventForm />}
+          />
         </Routes>
       </Container>
     </>
