844b87156f74e4ad4291358c65427bad2e317ec3
Leonardo Adrian Di Salvo <ledisalvo@gmail.com>
2023-01-27 12:30:03 -0300

    Add event filters component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import 'semantic-ui-css/semantic.min.css';
+import 'react-toastify/dist/ReactToastify.min.css';
+import 'react-calendar/dist/Calendar.css';
 import './app/layout/styles.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import RouteHandler from './app/layout/RouteHandler';
 import { Provider } from 'react-redux';
 import configStore from './app/store/configStore';
-import 'react-toastify/dist/ReactToastify.min.css';
 import { loadEvents } from './features/events/eventActions';
 
 const store = configStore();
